8e1ddd84d03aaad64a0db0d11938315e7a22f0fa
Alvin Uchenna <43749581+Veri5ied@users.noreply.github.com>
2022-01-03 20:05:27 +0100

    refactoring

--- src/index.js
+++ src/index.js
@@ -1,11 +1,32 @@
-import http from "http";
+import express, { json, urlencoded } from "express";
+import cors from "cors";
+import morgan from "morgan";
+import dotenv from "dotenv";
 
-const server = http
-  .createServer((req, res) => {
-    res.end("Hello from the server");
-  })
-  .listen(4001);
+dotenv.config();
 
-console.log("Server is up and running");
+const app = express();
+const PORT = process.env.PORT || 4200;
 
-export default server;
+app.disable("x-powered-by");
+
+app.use(cors());
+app.use(json());
+app.use(urlencoded({ extended: true }));
+app.use(morgan("dev"));
+
+app.use((req, res) => {
+  res.send("Hello from the server");
+});
+
+const start = () => {
+  try {
+    app.listen(PORT, () => {
+      console.log(`Solana API is serving on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+start();
